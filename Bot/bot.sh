52f558d65145c3cbb720eca8b7a3698181544b80
scriswan <vpnscriswan293@gmail.com>
2024-08-13 11:48:32 +0700

    Update bot.sh

--- Bot/bot.sh
+++ Bot/bot.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 function checking_sc() {
     MYIP=$(curl -sS ipv4.icanhazip.com)
-    IZIN=$(curl -sS https://raw.githubusercontent.com/lunatixmyscript/lunaip/main/ip | awk '{print $4}')
+    IZIN=$(curl -sS https://raw.githubusercontent.com/scriswan/lunaip/main/ip | awk '{print $4}')
 
     if echo "$IZIN" | grep -wq "$MYIP"; then
         echo "IZIN DITERIMA ANJING"
@@ -36,7 +36,7 @@ apt install python3 python3-pip -y
 apt install sqlite3 -y
 cd /media/
 rm -rf cybervpn
-wget https://raw.githubusercontent.com/lunatixmyscript/lunatixvpn/main/Bot/cybervpn.zip
+wget https://raw.githubusercontent.com/scriswan/v1/main/Bot/cybervpn.zip
 unzip cybervpn.zip
 cd cybervpn
 rm var.txt
@@ -110,86 +110,86 @@ systemctl enable cybervpn
 clear
 echo "downloading asset"
 
-wget -q -O /usr/bin/panelbot "https://raw.githubusercontent.com/lunatixmyscript/lunatixvpn/main/Bot/panelbot.sh" && chmod +x /usr/bin/panelbot
+wget -q -O /usr/bin/panelbot "https://raw.githubusercontent.com/scriswan/v1/main/Bot/panelbot.sh" && chmod +x /usr/bin/panelbot
 
 
 
 
 
-wget -q -O /usr/bin/addnoobz "https://raw.githubusercontent.com/lunatixmyscript/lunatixvpn/main/Bot/addnoobz.sh" && chmod +x /usr/bin/addnoobz
+wget -q -O /usr/bin/addnoobz "https://raw.githubusercontent.com/scriswan/v1/main/Bot/addnoobz.sh" && chmod +x /usr/bin/addnoobz
 
 
 
-wget -q -O /media/log-install.txt "https://raw.githubusercontent.com/lunatixmyscript/lunatixvpn/main/Bot/log-install.txt"
+wget -q -O /media/log-install.txt "https://raw.githubusercontent.com/scriswan/v1/main/Bot/log-install.txt"
 
 
 
-wget -q -O /usr/bin/add-vless "https://raw.githubusercontent.com/lunatixmyscript/lunatixvpn/main/Bot/add-vless.sh" && chmod +x /usr/bin/add-vless
+wget -q -O /usr/bin/add-vless "https://raw.githubusercontent.com/scriswan/v1/main/Bot/add-vless.sh" && chmod +x /usr/bin/add-vless
 
-wget -q -O /usr/bin/addtr "https://raw.githubusercontent.com/lunatixmyscript/lunatixvpn/main/Bot/addtr.sh" && chmod +x /usr/bin/addtr
-
-
-wget -q -O /usr/bin/addws "https://raw.githubusercontent.com/lunatixmyscript/lunatixvpn/main/Bot/addws.sh" && chmod +x /usr/bin/addws
+wget -q -O /usr/bin/addtr "https://raw.githubusercontent.com/scriswan/v1/main/Bot/addtr.sh" && chmod +x /usr/bin/addtr
 
 
-wget -q -O /usr/bin/addss "https://raw.githubusercontent.com/lunatixmyscript/lunatixvpn/main/Bot/addss.sh" && chmod +x /usr/bin/addss
-
-wget -q -O /usr/bin/cek-ssh "https://raw.githubusercontent.com/lunatixmyscript/lunatixvpn/main/Bot/cek-ssh.sh" && chmod +x /usr/bin/cek-ssh
+wget -q -O /usr/bin/addws "https://raw.githubusercontent.com/scriswan/v1/main/Bot/addws.sh" && chmod +x /usr/bin/addws
 
 
+wget -q -O /usr/bin/addss "https://raw.githubusercontent.com/scriswan/v1/main/Bot/addss.sh" && chmod +x /usr/bin/addss
 
-wget -q -O /usr/bin/cek-ss "https://raw.githubusercontent.com/lunatixmyscript/lunatixvpn/main/Bot/cek-ss.sh" && chmod +x /usr/bin/cek-ss
+wget -q -O /usr/bin/cek-ssh "https://raw.githubusercontent.com/scriswan/v1/main/Bot/cek-ssh.sh" && chmod +x /usr/bin/cek-ssh
 
 
 
-
-wget -q -O /usr/bin/cek-tr "https://raw.githubusercontent.com/lunatixmyscript/lunatixvpn/main/Bot/cek-tr.sh" && chmod +x /usr/bin/cek-tr
-
-wget -q -O /usr/bin/cek-vless "https://raw.githubusercontent.com/lunatixmyscript/lunatixvpn/main/Bot/cek-vless.sh" && chmod +x /usr/bin/cek-vless
-
-wget -q -O /usr/bin/cek-ws "https://raw.githubusercontent.com/lunatixmyscript/lunatixvpn/main/Bot/cek-ws.sh" && chmod +x /usr/bin/cek-ws
-
-wget -q -O /usr/bin/del-vless "https://raw.githubusercontent.com/lunatixmyscript/lunatixvpn/main/Bot/del-vless.sh" && chmod +x /usr/bin/del-vless
+wget -q -O /usr/bin/cek-ss "https://raw.githubusercontent.com/scriswan/v1/main/Bot/cek-ss.sh" && chmod +x /usr/bin/cek-ss
 
 
 
 
-wget -q -O /usr/bin/cek-noobz "https://raw.githubusercontent.com/lunatixmyscript/lunatixvpn/main/Bot/cek-noobz.sh" && chmod +x /usr/bin/cek-noobz
+wget -q -O /usr/bin/cek-tr "https://raw.githubusercontent.com/scriswan/v1/main/Bot/cek-tr.sh" && chmod +x /usr/bin/cek-tr
+
+wget -q -O /usr/bin/cek-vless "https://raw.githubusercontent.com/scriswan/v1/main/Bot/cek-vless.sh" && chmod +x /usr/bin/cek-vless
+
+wget -q -O /usr/bin/cek-ws "https://raw.githubusercontent.com/scriswan/v1/main/Bot/cek-ws.sh" && chmod +x /usr/bin/cek-ws
+
+wget -q -O /usr/bin/del-vless "https://raw.githubusercontent.com/scriswan/v1/main/Bot/del-vless.sh" && chmod +x /usr/bin/del-vless
 
 
 
-wget -q -O /usr/bin/deltr "https://raw.githubusercontent.com/lunatixmyscript/lunatixvpn/main/Bot/deltr.sh" && chmod +x /usr/bin/deltr
 
-wget -q -O /usr/bin/delws "https://raw.githubusercontent.com/lunatixmyscript/lunatixvpn/main/Bot/delws.sh" && chmod +x /usr/bin/delws
-
-
-
-wget -q -O /usr/bin/delss "https://raw.githubusercontent.com/lunatixmyscript/lunatixvpn/main/Bot/delss.sh" && chmod +x /usr/bin/delss
+wget -q -O /usr/bin/cek-noobz "https://raw.githubusercontent.com/scriswan/v1/main/Bot/cek-noobz.sh" && chmod +x /usr/bin/cek-noobz
 
 
 
-wget -q -O /usr/bin/renew-ss "https://raw.githubusercontent.com/lunatixmyscript/lunatixvpn/main/Bot/renew-ss.sh" && chmod +x /usr/bin/renew-ss
+wget -q -O /usr/bin/deltr "https://raw.githubusercontent.com/scriswan/v1/main/Bot/deltr.sh" && chmod +x /usr/bin/deltr
 
-
-wget -q -O /usr/bin/renewtr "https://raw.githubusercontent.com/lunatixmyscript/lunatixvpn/main/Bot/renewtr.sh" && chmod +x /usr/bin/renewtr
-
-
-wget -q -O /usr/bin/renewvless "https://raw.githubusercontent.com/lunatixmyscript/lunatixvpn/main/Bot/renewvless.sh" && chmod +x /usr/bin/renewvless
-
-
-wget -q -O /usr/bin/renewws "https://raw.githubusercontent.com/lunatixmyscript/lunatixvpn/main/Bot/renewws.sh" && chmod +x /usr/bin/renewws
-
-
-wget -q -O /usr/bin/cek-mws "https://raw.githubusercontent.com/lunatixmyscript/lunatixvpn/main/Bot/cek-mws.sh" && chmod +x /usr/bin/cek-mws
-
-wget -q -O /usr/bin/cek-mvs "https://raw.githubusercontent.com/lunatixmyscript/lunatixvpn/main/Bot/cek-mvs.sh" && chmod +x /usr/bin/cek-mvs
-
-
-wget -q -O /usr/bin/cek-mss "https://raw.githubusercontent.com/lunatixmyscript/lunatixvpn/main/Bot/cek-mss.sh" && chmod +x /usr/bin/cek-mss
+wget -q -O /usr/bin/delws "https://raw.githubusercontent.com/scriswan/v1/main/Bot/delws.sh" && chmod +x /usr/bin/delws
 
 
 
-wget -q -O /usr/bin/cek-mts "https://raw.githubusercontent.com/lunatixmyscript/lunatixvpn/main/Bot/cek-mts.sh" && chmod +x /usr/bin/cek-mts
+wget -q -O /usr/bin/delss "https://raw.githubusercontent.com/scriswan/v1/main/Bot/delss.sh" && chmod +x /usr/bin/delss
+
+
+
+wget -q -O /usr/bin/renew-ss "https://raw.githubusercontent.com/scriswan/v1/main/Bot/renew-ss.sh" && chmod +x /usr/bin/renew-ss
+
+
+wget -q -O /usr/bin/renewtr "https://raw.githubusercontent.com/scriswan/v1/main/Bot/renewtr.sh" && chmod +x /usr/bin/renewtr
+
+
+wget -q -O /usr/bin/renewvless "https://raw.githubusercontent.com/scriswan/v1/main/Bot/renewvless.sh" && chmod +x /usr/bin/renewvless
+
+
+wget -q -O /usr/bin/renewws "https://raw.githubusercontent.com/scriswan/v1/main/Bot/renewws.sh" && chmod +x /usr/bin/renewws
+
+
+wget -q -O /usr/bin/cek-mws "https://raw.githubusercontent.com/scriswan/v1/main/Bot/cek-mws.sh" && chmod +x /usr/bin/cek-mws
+
+wget -q -O /usr/bin/cek-mvs "https://raw.githubusercontent.com/scriswan/v1/main/Bot/cek-mvs.sh" && chmod +x /usr/bin/cek-mvs
+
+
+wget -q -O /usr/bin/cek-mss "https://raw.githubusercontent.com/scriswan/v1/main/Bot/cek-mss.sh" && chmod +x /usr/bin/cek-mss
+
+
+
+wget -q -O /usr/bin/cek-mts "https://raw.githubusercontent.com/scriswan/v1/main/Bot/cek-mts.sh" && chmod +x /usr/bin/cek-mts
 
 cp /tmp/var.txt /media/cybervpn
 rm -rf bot.sh
